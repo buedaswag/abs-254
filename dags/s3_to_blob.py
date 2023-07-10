11c40e303e09c3ea7d224ed4624cec7a5b7a1a1c
Miguel Dias <miguel.dias@relexsolutions.com>
2023-07-10 15:13:11 +0100

    updating code

--- dags/s3_to_blob.py
+++ dags/s3_to_blob.py
@@ -5,7 +5,6 @@ import os
 import boto3
 from azure.storage.blob import BlobClient
 
-
 # AWS credentials from environment variables
 aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
@@ -13,22 +12,28 @@ aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 # Azure Storage blob SAS URL from environment variables
 blob_sas_url = os.environ['AZURE_BLOB_SAS_URL']
 
-# Local file path
-local_file_path = '/tmp/my_file'
+# S3 bucket and file details
+s3_bucket = 'my-s3-bucket'
+s3_file_key = 'my-s3-object-key'
 
+# Azure Blob details
+blob_name = 'my-blob-name'
+
+# Local file path
+local_file_path = '/tmp/test.txt'
 
 def transfer_s3_to_blob():
     s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     # Download the file from S3 to the local file system
-    s3_client.download_file('my-s3-bucket', 'my-s3-object-key', local_file_path)
+    s3_client.download_file(s3_bucket, s3_file_key, local_file_path)
 
     # Create a blob client using the blob SAS URL
     blob_client = BlobClient.from_blob_url(blob_sas_url)
 
     print("\nUploading to Azure Storage as blob:\n\t" + local_file_path)
 
-    # Upload the created file
+    # Upload the downloaded file
     with open(local_file_path, "rb") as data:
         blob_client.upload_blob(data, overwrite=True)
 
