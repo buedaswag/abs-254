160d51feec6a3e54477738fc9304f43b7a0e82c9
Miguel Dias <miguel.dias@relexsolutions.com>
2023-07-11 09:43:18 +0100

    update requirements

--- dags/s3_to_blob.py
+++ dags/s3_to_blob.py
@@ -3,7 +3,10 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 import os
 import boto3
-from azure.storage.blob import BlobClient, BlobServiceClient
+from azure.storage.blob import BlobServiceClient
+from azure.identity import ClientSecretCredential
+from azure.keyvault.secrets import SecretClient
+
 
 version = "349309f"
 
@@ -15,15 +18,18 @@ aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 blob_sas_url = os.environ['AZURE_BLOB_SAS_URL']
 
 # S3 bucket and file details
-s3_bucket = 'rlxabs254test'
+s3_bucket = 'rlxabs254k8stest'
 file_name = 'test.txt'
 
 # Azure Blob details
-storage_account_name = 'rlxabs254test'
-container_name = 'abs-254'
+key_vault_name = "kvrlx-abs254001-dev"
+sas_token_name = "sas-token-sa-rlxabs254001batchdev"
+storage_account_name = 'rlxabs254001batchdev'
+container_name = 'abs254001-dev-batch'
 
 # Local file path
 local_file_path = '/tmp/test.txt'
+
 
 def transfer_s3_to_blob():
     s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
@@ -34,10 +40,22 @@ def transfer_s3_to_blob():
     s3_client.download_file(s3_bucket, file_name, local_file_path)
 
     #########
+    # Get the creds from key vault
+    #########
+    credential = ClientSecretCredential(
+        tenant_id=os.environ["ARM_TENANT_ID"],
+        client_id=os.environ["ARM_CLIENT_ID"],
+        client_secret=os.environ["ARM_CLIENT_SECRET"]
+    )
+    vault_url = f"https://{key_vault_name}.vault.azure.net".format()
+    client = SecretClient(vault_url=vault_url, credential=credential)
+    sas_token = client.get_secret(sas_token_name).value
+
+    #########
     # Upload the file to Azure Blob
     #########
     account_url = (f"https://{storage_account_name}.blob.core.windows.net")
-    blob_service_client = BlobServiceClient(account_url, credential=blob_sas_url)
+    blob_service_client = BlobServiceClient(account_url, credential=sas_token)
 
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
 
